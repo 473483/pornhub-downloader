6e456fbb3e2715f312b7b62f9a18abbad1cd9df4
陈之 <chenzhi@chenzhideMacBook-Pro.local>
2020-02-03 23:35:10 +0800

    init project

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,14 +5,20 @@ module.exports = {
   },
   'extends': [
     'plugin:vue/essential',
-    '@vue/standard',
-    '@vue/typescript'
+    '@vue/standard'
   ],
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    // 不检测语句末尾的分号
+    'semi': ['off', 'always'],
+    // 强制缩进为2个空格
+    'indent': ['error', 2],
+    // 关闭函数名称跟括号之间的空格检测
+    'space-before-function-paren': 0,
+    // 忽略大括号内的空格
+    'object-curly-spacing': 0
   },
   parserOptions: {
-    parser: '@typescript-eslint/parser'
+    parser: 'babel-eslint'
   }
 }
