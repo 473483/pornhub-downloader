eb35802a7434aca829c926b841e63a24b4f21699
陈之 <chenzhi@chenzhideMacBook-Pro.local>
2020-02-03 23:58:40 +0800

    添加element-ui

---
+++ src/views/Index.vue
@@ -0,0 +1,17 @@
+<template>
+  <el-container direction="vertical">
+  </el-container>
+</template>
+
+<script lang="ts">
+  import { Vue, Component } from 'vue-property-decorator'
+
+  @Component({})
+  export default class Index extends Vue {
+
+  }
+</script>
+
+<style scoped>
+
+</style>
